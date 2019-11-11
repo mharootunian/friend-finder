3713be724a43bea318755add173e0565200cc402
Meero Harootunian <meero.harootunian@gmail.com>
2019-11-10 19:36:56 -0700

    added some router logic

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+const htmlRoutes = require("./app/routing/htmlRoutes");
+const apiRoutes = require("./app/routing/apiRoutes");
+
+app.use("/", htmlRoutes);
+app.use("/api/v1", apiRoutes);
+
+app.use(express.json());
+
