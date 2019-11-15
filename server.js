a8088ad34fc70070ffe0c6067ce844751c6fe4ed
Meero Harootunian <meero.harootunian@gmail.com>
2019-11-14 21:29:01 -0700

    fixed port

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ const path = require("path");
 const htmlRoutes = require("./app/routing/htmlRoutes");
 const apiRoutes = require("./app/routing/apiRoutes");
 
+const port = process.env.PORT || 8080;
+
 app.use("/", htmlRoutes);
 app.use("/api", apiRoutes);
 
 app.use(express.json());
 app.use(express.static("app/public"));
 
-const server = app.listen(8080, () => {
+const server = app.listen(port, () => {
     console.log(`Server started at ${server.address().address}:${server.address().port}`);
 });
